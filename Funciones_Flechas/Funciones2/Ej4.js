957270616f7e0dc6a0999c1a99bff892ea5f896a
albertororu <albertororu16@gmail.com>
2024-02-08 12:18:37 +0100

    ultimo cambio

--- Funciones_Flechas/Funciones2/Ej4.js
+++ Funciones_Flechas/Funciones2/Ej4.js
@@ -1,6 +1,7 @@
 const tablaMultiplicar = (j) => {
-    for (let i = 0; i < 10; i++) {
-        document.write(j + "x" + i + "=" + j * i)
+    for (let i = 0; i <= 10; i++) {
+        console.log(j + "x" + i + "=" + j * i)
     }
+
 }
-tablaMultiplicar(5)
+console.log(tablaMultiplicar(2))
